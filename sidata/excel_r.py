24bde9ff37bb3a668ea414323e607baa19d06377
W. Max Alexander <w.max.alexander@gmail.com>
2019-10-27 17:14:18 -0400

    Spec.

--- sidata/excel_r.py
+++ sidata/excel_r.py
@@ -7,11 +7,12 @@ from zipfile import BadZipFile
 
 class excel_reader:
     """
-    Reads an Excel file (.xls or .xlsx) as a list of lists; a wrapper
-    over the OpenPyXL interface.
+    Reads an Excel file (.xls or .xlsx) as a list of dictionaries; a
+    wrapper over the OpenPyXL interface.
 
     For files with multiple data sheets, a "sheet_name" parameter
     should be specified, otherwise the first sheet is chosen.
+
     """
 
     def __init__(self, target, sheet_name = None):
@@ -24,21 +25,22 @@ class excel_reader:
             self.sheet = self.workbook[sheets[0]]
 
         self.source = self.sheet.iter_rows()
-        self.header = next(self.source)
+        
+        self.header = next(self.__iter__())
 
     def __iter__(self):
         for row in self.source:
             values = [x.value for x in row]
             if not any(values):
-                break
+                continue
             yield values
 
     def close(self):
         # How to properly close an openpyxl sheet is still mostly
         # a matter of hearsay.
-        self.wb._archive.close()
+        self.workbook._archive.close()
         del self.sheet
-        del self.wb
+        del self.workbook
 
 
 class excel_writer:
@@ -64,7 +66,7 @@ class excel_writer:
             self.workbook = openpyxl.Workbook(write_only = True)
             if sheet_name and self.workbook.sheetnames:
                 self.workbook.remove(self.workbook.sheetnames[0])
-                self.sheet = self.workbook.create_sheet(sheet_name)
+            self.sheet = self.workbook.create_sheet(sheet_name)
 
         self.row_index = 1
 
@@ -74,14 +76,15 @@ class excel_writer:
         if isinstance(row, dict):
             row = [row[x] for x in self.columns]
 
-        for col_index, value in enumerate(row, start = 1):
-            item = self.sheet.cell(row = self.row_index,
-                                   column = col_index)
-            item.value = value
+        self.sheet.append(row)
+        # for col_index, value in enumerate(row, start = 1):
+        #     item = self.sheet.cell(row = self.row_index,
+        #                            column = col_index)
+        #     item.value = value
 
         self.row_index += 1
 
     def close(self):
-        self.wb.save(self.filename)
+        self.workbook.save(self.filename)
 
 
